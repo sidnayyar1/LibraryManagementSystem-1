fda998c66c4a026de4b4b46e5857767ad5fdf519
Ankita Pabbi <erankitapabbi@gmail.com>
2019-06-22 16:32:09 -0400

    [Ankita Pabbi ] Updated Librarian class with appropriate variables and initializer

--- LibraryManagementSystem/Employee/Librarian.swift
+++ LibraryManagementSystem/Employee/Librarian.swift
@@ -10,14 +10,29 @@ import Foundation
 class Librarian: Employees {
     
     var room_number: Int!
+    var book_array: [Books]
     
-    init() {
-        self.room_number = Int()
-       super.init(emp_id: <#T##Int#>, emp_f_name: <#T##String#>, emp_l_name: <#T##String#>, emp_email: <#T##String#>, emp_contact: <#T##Int#>, join_date: <#T##Date#>)
+    init(emp_id: Int, emp_f_name: String, emp_l_name: String, emp_email: String, emp_contact: Int, join_date: Date,room_num: Int, book_arr: [Books]) {
+        self.room_number = room_num
+        self.book_array = book_arr
+        super.init(emp_id: emp_id, emp_f_name: emp_f_name, emp_l_name: emp_l_name, emp_email: emp_email, emp_contact: emp_contact, join_date: join_date)
     }
+    
     override func display() -> String {
         var data = "data"
         return data
     }
+    
+    
+    func addBook() {
+        
+    }
+    func removeBook() {
+        
+    }
+    func updateBook() {
+        
+    }
+
     
 }
