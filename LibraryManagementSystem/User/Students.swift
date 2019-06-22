705314fe1013a8fd9629046b166f6182ce8cd598
Ankita Pabbi <erankitapabbi@gmail.com>
2019-06-22 16:39:43 -0400

    [Ankita Pabbi] Updated Student class with appropriate initializer

--- LibraryManagementSystem/User/Students.swift
+++ LibraryManagementSystem/User/Students.swift
@@ -11,14 +11,15 @@ import Foundation
 class Student: Users {
     
     var borrower: Dictionary<Int,String>
-    var student_enrollement_Number: Int!
-    var class_name: String!
+    var student_enrollement_Number: Int
+    var class_name: String
     
-   init() {
+    init(u_id: Int, f_name: String, l_name: String, email: String, addr: Address, ph_num: Int,borr: Dictionary<Int,String>,stu_en_num: Int,class_name: String) {
         
-        self.borrower = Dictionary<Int,String>()
-        self.student_enrollement_Number = Int()
-        self.class_name = String()
+        self.borrower = borr
+        self.student_enrollement_Number = stu_en_num
+        self.class_name = class_name
+        super.init(u_id: u_id, f_name: f_name, l_name: l_name, email: email, addr: addr, ph_num: ph_num)
        
     
     }
